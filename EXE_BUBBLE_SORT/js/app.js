50ccac9804dbfb148a0fc0bad157e2b2ad84f575
Samuel Amaro <samuelamaro2018@hotmail.com>
2022-03-02 16:43:39 -0300

    novo exercicio ded algoritmo de classificação bubble sort

---
+++ EXE_BUBBLE_SORT/js/app.js
@@ -0,0 +1,5 @@
+import { bubbleSort } from "./script.js";
+
+let array = [6, 5, 3, 1, 8, 7, 2, 4];
+
+console.log(bubbleSort(array));
---
+++ EXE_BUBBLE_SORT/js/script.js
@@ -0,0 +1,13 @@
+export function bubbleSort(arr) {
+    let array = arr;
+    for(let i = 0; i < array.length; i++) {
+        for(let j = i + 1; j < array.length; j++) {
+            if(array[i] > array[j]) {
+                let tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+            }
+        }
+    }
+    return array;
+}
